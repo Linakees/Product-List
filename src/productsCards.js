856f895614f8d4d83a3e01f74dce5d59c28329b3
Alina Trofimovica <95880212+Linakees@users.noreply.github.com>
2023-11-25 13:25:38 +0200

    Update productsCards.js

--- src/productsCards.js
+++ src/productsCards.js
@@ -3,37 +3,19 @@ import { Link } from "react-router-dom";
 
 function ProductCard (props){
    return( 
-   //  <div className="product-card">
-   //  <p className='name'>{props.name}</p>
-   //  <p className='price'>{props.price}</p>
-   //  <p className='currency'>{props.currency}</p>
-   //  <p className='category'>{props.category}</p>
-   //  <p className='description'>{props.description}</p>
-   //  <Link to={`/product/${props.id}`}>View Details</Link>
-   //  </div>
-
-         <Link to={`/product/${props.id}`}>
-      <div className="product-card">
-      <p className='name'>{props.name}</p>
-      <p className='price'>{props.price}</p>
-      <p className='currency'>{props.currency}</p>
-      <p className='category'>{props.category}</p>
-      <p className='description'>{props.description}</p>
-      </div>
+      <Link to={`/product/${props.id}`}>
+         <div className="product-card">
+            <p className='name'>{props.name}</p>
+            <p className='price'>{props.price}</p>
+            <p className='currency'>{props.currency}</p>
+            <p className='category'>{props.category}</p>
+            <p className='description'>{props.description}</p>
+         </div>
       </Link> 
    );
 }
 
 export default ProductCard;
 
-{/* <Link to={`/product/${props.id}`}>
-<div className="product-card">
-<p className='name'>{props.name}</p>
-<p className='price'>{props.price}</p>
-<p className='currency'>{props.currency}</p>
-<p className='category'>{props.category}</p>
-<p className='description'>{props.description}</p>
-</div>
-</Link> */}
 
 
